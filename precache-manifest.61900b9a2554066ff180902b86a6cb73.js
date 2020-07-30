eb2b53439d83bde9e9345bbe1561e03ebf3a691c
BradMoore <48004534+developingdeveloper0@users.noreply.github.com>
2020-07-30 13:55:41 -0400

    Updates

---
+++ precache-manifest.61900b9a2554066ff180902b86a6cb73.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c43915be0f5225b8504f8a2f1b2b7ae6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "11d9c337934d9a99e621",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "99904ee7fcbf07e6220f",
+    "url": "/static/css/main.fc9db48e.chunk.css"
+  },
+  {
+    "revision": "11d9c337934d9a99e621",
+    "url": "/static/js/2.3316453f.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/static/js/2.3316453f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99904ee7fcbf07e6220f",
+    "url": "/static/js/main.bb3e9e52.chunk.js"
+  },
+  {
+    "revision": "8fae2f0bf44244f6ee82",
+    "url": "/static/js/runtime-main.14ada470.js"
+  }
+]);
